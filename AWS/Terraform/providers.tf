5ba8a7e4d1d690205b3c7de5ace358f067227e87
Mevalal Saroj <sarojmeva@gmail.com>
2025-09-30 13:52:27 +0530

    Update providers.tf

--- AWS/Terraform/providers.tf
+++ AWS/Terraform/providers.tf
@@ -1,34 +1,53 @@
 
 #########################################
 terraform {
-  required_version = ">= 1.3.2"
+  required_version = ">= 1.5.7"
   required_providers {
-    aws = {
+  
+  aws = {
       source  = "hashicorp/aws"
       version = ">= 6.0"
+      #configuration_aliases = [ aws.nwinfra ]
     }
-    tls = {
+   tls = {
       source  = "hashicorp/tls"
-      version = ">= 4.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
+      version = ">= 3.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.13.0"
-    }
+      version = ">= 0.9"
+    }	
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0.0"
+    }	
+    opensearch = {
+      source  = "opensearch-project/opensearch"
+      version = ">= 2.0"
+    }	
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = ">= 1.35.0"
+    }	
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0"
+      version = ">= 2.10"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7"
     }
   }
 }
 
 provider "aws" {
   region = "ap-south-1"
+  alias  = "mumbai"
+}
+
+provider "aws" {
+  region = "ap-south-1"
   assume_role {
-    role_arn = "arn:aws:iam::048599826367:role/hbl-aws-role-tfeappinfra-eligibility-engine-uat"
+    role_arn = "arn:aws:iam::216066832707:role/hbl-aws-role-tfeappinfra-sharedservices-infra-uat"
   }
 }
