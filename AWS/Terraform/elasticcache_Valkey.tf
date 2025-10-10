acb2a689aad22d8a365db7749cbeb26087c20121
Mevalal Saroj <sarojmeva@gmail.com>
2025-10-10 10:10:07 +0530

    Update elasticcache_Valkey.tf

--- AWS/Terraform/elasticcache_Valkey.tf
+++ AWS/Terraform/elasticcache_Valkey.tf
@@ -11,7 +11,7 @@ module "elasticache_valkey" {
   kms_key_arn = "arn:aws:kms:ap-south-1:911372318716:key/mrk-d77d91e1dca246c6a8d534a2b48b3e39"
   auth_token                 = "HdfcBankLimited123$"
 
-  maintenance_window         = "sun:05:00-sun:09:00"
+  maintenance_window         = "sun:01:00-sun:04:00"
   apply_immediately          = true
 
   # Security Group
@@ -28,14 +28,14 @@ module "elasticache_valkey" {
   create_parameter_group      = false
   
 # Muti-AZ
-  multi_az_enabled=true
-  automatic_failover_enabled=true
-  num_cache_nodes=2
-  preferred_availability_zones=["ap-south-1a","ap-south-1b","ap-south-1c"]
+  multi_az_enabled = true
+  automatic_failover_enabled = true
+  num_cache_nodes = 2
+  preferred_availability_zones = ["ap-south-1a","ap-south-1b","ap-south-1c"]
 
 # Backup
-  snapshot_retention_limit=7
-  snapshot_window      = "05:00-06:00"
+  snapshot_retention_limit = 7
+  snapshot_window  = "05:00-06:00"
   
 tags = merge(var.additional_tags, {
     Name = join("-", [local.org, local.csp, local.region, local.account, local.vpcname, local.env, "valkey"])
