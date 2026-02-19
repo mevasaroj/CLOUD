1a03ab21b00a96b939c8078c1b1cf44e173f5720
Mevalal Saroj <sarojmeva@gmail.com>
2026-02-19 10:32:44 +0530

    Update nlb-to-alb.tf

--- AWS/Terraform/nlb-to-alb.tf
+++ AWS/Terraform/nlb-to-alb.tf
@@ -1,105 +1,104 @@
-##################################################################
-# Start of Network Load Balancer
-##################################################################
-
-module "nonpcidss-nlb" {
-  source = "terraform-aws-modules/alb/aws"
-  name   = join("-", [local.org, local.csp, local.account, "nonpcidss-nlb"])  # - Max 32 Characters
-  load_balancer_type         = "network"
-  vpc_id                     = var.nonpcidss-prod-vpc
-  internal                   = true
-  preserve_host_header       = false
-  drop_invalid_header_fields = false
-  enable_deletion_protection = true
-  desync_mitigation_mode     = "defensive"
+#============================================================
+### START OF ALB ####
+#============================================================
+module "app-nlb" {
+  source              = "terraform.hdfcbank.com/HDFCBANK/module/aws//modules/aws-load-balancer_v6"
+  name                = join("-", [local.org, local.csp, local.region, local.vpcname, "app-nlb"])  # - Max 32 Characters
+  vpc_id              = var.nonpcidss-prod-vpc
+  internal            = true
+  load_balancer_type  = "network"  
+  drop_invalid_header_fields = true
   enable_cross_zone_load_balancing = true
-  dns_record_client_routing_policy = "availability_zone_affinity"
-
-  # Use `subnet_mapping` to attach IPs
-  subnet_mapping = [
-    {
-      subnet_id = var.infra-subnet-aza
-      private_ipv4_address = "10.196.212.11"
-    },
-    {
-      subnet_id = var.infra-subnet-azb
-      private_ipv4_address = "10.196.212.80"
-    },
-    {
-      subnet_id = var.infra-subnet-azc
-      private_ipv4_address = "10.196.212.139" 
-   }
-  ]
-
+  enable_deletion_protection = true
+  preserve_host_header = true
+  
   # Security Group
   create_security_group = false
   security_groups     = ["${var.app-alb-sg}"]
 
+  # Subnet Mapping
+  subnet_mapping = [
+    {
+      subnet_id = var.infra-subnet-aza
+      private_ipv4_address = "10.213.12.10"
+    },
+    {
+      subnet_id = var.infra-subnet-azb
+      private_ipv4_address = "10.213.12.138"
+    },
+    {
+      subnet_id = var.infra-subnet-azc
+      private_ipv4_address = "10.213.13.10"
+   }
+  ]
+     
 
-  # Access Log
   access_logs         = {
-    bucket ="hbl-aws-aps1-nonpcidss-prod-dlm-load-balancer-log-bucket"
-    prefix = "nonpcidss-nlb"
+    bucket = "hbl-aws-aps1-nonpcidss-uat-xcl-load-balancer-log-bucket"
+    prefix = join("-", [local.org, local.csp, local.region, local.vpcname, "app-nlb"])
     enabled = true
   }
 
+
 #=========================================================================================
+  
   # START OF listeners
   # -------------------------------------------
   listeners = {
-    nonpcidss-listeners = {
-      port                     = 443
-      protocol                 = "TCP"
-      tcp_idle_timeout_seconds = 60
+    ex-tcp = {
+      port            = 443
+      protocol        = "TCP"
+      target_group_index = 0
+      load_balancer_arn  = "arn:aws:elasticloadbalancing:ap-south-1:972742752662:loadbalancer/app/k8s-ekstest-applycar-7849a4e025/f1f4736e61b949f2"
       forward = {
-        target_group_key = "nonpcidss-nlb-tg"
+        target_group_key = "nlb-tg"
       }
+
     }
   }
   
-  # END OF listeners
+ # END OF listeners
+ 
 #=========================================================================================
-
   # START OF Target Group
-  # ------------------------------------------
+  # -------------------------------------------
   target_groups = {
-    nonpcidss-nlb-tg = {
-      name                 = join("-", [local.org, local.csp, local.account, "nonpcidss-nlb-tg"])
-      protocol             = "TCP"
-      port                 = 443
-      target_type          = "alb"
-      preserve_client_ip   = true
-      target_id            = "arn:aws:elasticloadbalancing:ap-south-1:686255955923:loadbalancer/app/hbl-aws-dlm-nonpcidss-prod-alb/84c5a8d8fe47a504"
-      deregistration_delay = 10
-	  
+    nlb-tg = {
+      namex       = join("-", [local.org, local.csp, local.region, local.vpcname, "nlb-tg"])
+      protocol    = "TCP"
+      port        = 443
+      target_type = "alb"
+      preserve_client_ip = true
+      target_id   = "arn:aws:elasticloadbalancing:ap-south-1:972742752662:loadbalancer/app/k8s-ekstest-applycar-7849a4e025/f1f4736e61b949f2"
+
       health_check = {
-        enabled             = true
-        interval            = 30
         path                = "/"
-        port                = "traffic-port"
+        enabled             = true
+        interval            = 30        
+        port                = 443
         healthy_threshold   = 3
-        unhealthy_threshold = 3
-        timeout             = 6
+        unhealthy_threshold = 5
+        timeout             = 5
+        protocol            = "HTTPS"
+        matcher             = "200-450"
       }
-
-      # nonpcidss-nlb-tg Tags
-      tags = merge({Name = "${join("-", [local.org, local.csp, local.account, "nonpcidss-nlb-tg"])}",
-        ProvisioningDate = "03-March-2025"},
-        var.additional_tags)
-	  
+      # Target Group Tags  
+   tags = merge({Name = "${join("-", [local.org, local.csp, local.region, local.account, local.vpcname, local.env, "nlb-tg"])}"}, 
+    var.additional_tags)
+		
     }
-
+	
   }
   # END OF Target Group
-
 #=========================================================================================
 
-  # LB TAGS
-  tags = merge({Name = "${join("-", [local.org, local.csp, local.account, "nonpcidss-nlb"])}",
-    ProvisioningDate = "03-March-2025"},
+
+# ALB TAGS
+tags = merge({Name = "${join("-", [local.org, local.csp, local.region, local.account, local.vpcname, local.env, "app-nlb"])}"}, 
     var.additional_tags)
+  
 }
 
-################################################################################
-# End of Network Load Balancer
-################################################################################
+#============================================================
+### END OF ALB ####
+#============================================================
