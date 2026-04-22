5133f33581ba96e5f39e46b79c1d36a40b3298dc
Mevalal Saroj <sarojmeva@gmail.com>
2026-04-22 15:06:27 +0530

    Update ec_start.py

--- Python/Python_Code_Lambda/ec_start.py
+++ Python/Python_Code_Lambda/ec_start.py
@@ -1,1 +1,11 @@
-test
+import json
+import boto3
+ 
+ec2 = boto3.resource('ec2', region_name='ap-south-1')
+def lambda_handler(event, context):
+   instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']},{'Name': 'tag:AutoRestart','Values':['True']}])
+   for instance in instances:
+      id=instance.id
+      ec2.instances.filter(InstanceIds=[id]).start()
+      print("Instance ID is Started:- "+instance.id)
+   return "success"
