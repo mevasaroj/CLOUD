9bb7856694c2231c93667c3cd49ad09b84524991
Mevalal Saroj <sarojmeva@gmail.com>
2026-04-27 10:34:41 +0530

    Update ec2_second_account_nwinfra.tf

--- AWS/Terraform/ec2_second_account_nwinfra.tf
+++ AWS/Terraform/ec2_second_account_nwinfra.tf
@@ -17,22 +17,17 @@ module "hbl-aws-aps1-vkyc-nonpcidss-prod-turn-ec2-01" {
   key_name                = "hbl-aws-aps1-nwinfra-inbound-media-prod-vkyc-kp"
   metadata_options        = var.metadata_options
   monitoring              = false
-  disable_api_termination = true
+  #disable_api_termination = true
   enable_volume_tags      = true
  # iam_instance_profile    = "hbl-aws-nwinfra-ssm-ec2care-role"
-  private_ip              = "10.199.92.203"
-  volume_tags = merge(
-    {
-      application = "turn-ec2"
-    }, 
-    var.additional_tags
-  )
-  tags = merge(
-    {
+  #private_ip              = "10.199.92.203"
+  volume_tags = merge( { application = "turn-ec2" }, 
+    var.additional_tags )
+
+  tags = merge( {
       Name = join("-", [local.org, local.csp, local.region, local.vpcname, local.env, local.account, "turn-ec2"])
-    }, 
-    var.additional_tags
-  )
+    },  var.additional_tags )
+
 }
 
 module "hbl-aws-aps1-vkyc-nonpcidss-prod-turn-ec2-01_ebs_01" {
@@ -56,9 +51,6 @@ module "hbl-aws-aps1-vkyc-nonpcidss-prod-turn-ec2-01_ebs_01" {
 ########### 01.START OF HBL-AWS-APS1-VKYC-NONPCIDSS-PROD-TURN-EC2-01    ##############################################
 ##################################################################################################################################################
   
-  ##################################################################################################################################################
-########### REMOTE VPC, SUBNET and SECURITY GROUP VALUES DECLARATION     ##############################################
-##################################################################################################################################################
 
 ##################################################################################################################################################
 ########### 01.START OF HBL-AWS-APS1-VKYC-NONPCIDSS-PROD-TURN-EC2-02     ##############################################
@@ -70,27 +62,23 @@ module "hbl-aws-aps1-vkyc-nonpcidss-prod-turn-ec2-02" {
   subnet_id               = var.nwinfra-inbound-media-prod-stun-turn-subnet-azb
   vpc_security_group_ids  = ["${var.nwinfra-media-inbound-turn-server-sg}", "${var.nwinfra-inbound-media-prod-CommonInfraRule-sg}"]
   ami                     = "ami-035e3b84134467696"     # Turn Server AMI Available in NWINFRA Account 
-  #ami = "ami-022ce6f32988af5fa"  #Mkt Place AMI
   instance_type           = "c6a.large"
   key_name                = "hbl-aws-aps1-nwinfra-inbound-media-prod-vkyc-kp"
   metadata_options        = var.metadata_options
   monitoring              = false
-  disable_api_termination = true
+  #disable_api_termination = true
   enable_volume_tags      = true
   #iam_instance_profile    = "hbl-aws-nwinfra-ssm-ec2care-role"
-  private_ip              = "10.199.92.214"
-  volume_tags = merge(
-    {
-      application = "turn-ec2-02"
-    }, 
-    var.additional_tags
-  )
-  tags = merge(
-    {
+  #private_ip              = "10.199.92.214"
+
+  enable_volume_tags      = true
+  volume_tags = merge( { application = "turn-ec2-02" }, 
+    var.additional_tags  )
+
+  tags = merge( {
       Name = join("-", [local.org, local.csp, local.region, local.vpcname, local.env, local.account, "turn-ec2-02"])
-    }, 
-    var.additional_tags
-  )
+    },     var.additional_tags  )
+
 }
 ##################################################################################################################################################
 ########### 01.START OF HBL-AWS-APS1-VKYC-NONPCIDSS-PROD-TURN-EC2-02    ##############################################
@@ -106,16 +94,19 @@ module "turn-ec2-03" {
   key_name                = "hbl-aws-aps1-nwinfra-inbound-media-prod-vkyc-kp"
   metadata_options        = var.metadata_options
   monitoring              = false
-  disable_api_termination = true
-  enable_volume_tags      = true
+  #disable_api_termination = true
   #iam_instance_profile    = "hbl-aws-nwinfra-ssm-ec2care-role"
  # private_ip              = "10.199.92.214"
+
+  enable_volume_tags      = true
   volume_tags = merge(
     { application = "turn-ec2-03"
     },  var.additional_tags)
+
   tags = merge(
     {  Name = join("-", [local.org, local.csp, local.region, local.vpcname, local.env, local.account, "turn-ec2-03"])
     }, var.additional_tags )
+
 }
 ##################################################################################################################################################
   module "turn-ec2-04" {
@@ -130,14 +121,16 @@ module "turn-ec2-03" {
   metadata_options        = var.metadata_options
   monitoring              = false
   disable_api_termination = true
-  enable_volume_tags      = true
   #iam_instance_profile    = "hbl-aws-nwinfra-ssm-ec2care-role"
  # private_ip              = "10.199.92.214"
-  volume_tags = merge(
-    { application = "turn-ec2-03"
-    },  var.additional_tags)
+
+  enable_volume_tags      = true
+  volume_tags = merge( { application = "turn-ec2-04"  },  
+        var.additional_tags )
+
   tags = merge(
     {  Name = join("-", [local.org, local.csp, local.region, local.vpcname, local.env, local.account, "turn-ec2-04"])
     }, var.additional_tags )
+
 }
 ##################################################################################################################################################
